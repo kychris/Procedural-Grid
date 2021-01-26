7efa84af00d849455ca19c0673577810fe17ec5a
kychris <kychris1999@hotmail.com>
2021-01-25 17:48:28 -0800

    initial upload

---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class Grid : MonoBehaviour
+{
+
+    public int xSize, ySize;
+
+    private Vector3[] vertices;
+    private Mesh mesh;
+
+    private void Awake()
+    {
+        Generate();
+
+    }
+
+    private void Generate()
+    {
+        //creates new mesh
+        GetComponent<MeshFilter>().mesh = mesh = new Mesh();
+        mesh.name = "Procedural Grid";
+
+        //need one more vertice than height and width
+        vertices = new Vector3[(xSize + 1) * (ySize + 1)];
+        Vector2[] uv = new Vector2[vertices.Length];
+        Vector4[] tangents = new Vector4[vertices.Length];
+        Vector4 tangent = new Vector4(1f, 0f, 0f, -1f);
+
+        //advanced loop syntax that I did not come up with
+        for (int i = 0, y = 0; y <= ySize; y++)
+        {
+            for (int x = 0; x <= xSize; x++, i++)
+            {
+                vertices[i] = new Vector3(x, y);
+                uv[i] = new Vector2((float)x / xSize, (float)y / ySize);
+                tangents[i] = tangent; //same tangent for every vertice
+            }
+        }
+        mesh.vertices = vertices;
+        mesh.uv = uv;
+        mesh.tangents = tangents;
+
+
+        //define mesh triangles
+        int[] triangles = new int[xSize * ySize * 6];
+        for (int ti = 0, vi = 0, y = 0; y < ySize; y++, vi++)
+        {
+            for (int x = 0; x < xSize; x++, ti += 6, vi++)
+            {
+                triangles[ti] = vi;
+                triangles[ti + 3] = triangles[ti + 2] = vi + 1;
+                triangles[ti + 4] = triangles[ti + 1] = vi + xSize + 1;
+                triangles[ti + 5] = vi + xSize + 2;
+                mesh.triangles = triangles;
+                mesh.RecalculateNormals(); //auto normal from triangles
+            }
+        }
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (vertices == null)
+        {
+            return;
+        }
+        Gizmos.color = Color.black;
+
+        for (int i = 0; i < vertices.Length; i++)
+        { //draw sphere for each vertice
+            Gizmos.DrawSphere(vertices[i], 0.1f);
+        }
+    }
+}
